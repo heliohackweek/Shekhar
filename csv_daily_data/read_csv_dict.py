e30e63117cc611bd574e8ceafcb5de3a23189110
Mykhaylo Mike Shumko <msshumko@gmail.com>
2020-08-25 12:22:17 -0600

    Added pyc and env to gitignore and wrote the csv reader for the MetOP files.

--- csv_daily_data/read_csv_dict.py
+++ csv_daily_data/read_csv_dict.py
@@ -1,4 +1,42 @@
 import csv
-with open("filename.csv") as csv_file:
-    reader = csv.reader(csv_file)
-    mydict = dict(reader)
+from datetime import datetime, date
+import re
+
+import numpy as np
+import pandas as pd
+
+def load_meped_csv(file_name, as_df=True):
+    """
+    Load the meped data into a dictionary or a pandas DataFrame.
+    """
+
+    numeric_date = re.findall(r'\d+', file_name)[0]
+    date_obj = datetime.strptime(numeric_date[2:], '%Y%m%d')
+
+    with open(file_name) as csv_file:
+        reader = csv.reader(csv_file)
+        mydict = dict(reader)
+
+        for key, val in mydict.items():
+            # Remove all of the non-numeric characters
+            val = val.replace('\n', '')
+            val = val.replace('[', '')
+            val = val.replace(']', '')
+
+            mydict[key] = np.array(val.split()).astype(float)
+
+        if as_df:
+            df = pd.DataFrame(data=mydict)
+            df['dateTime'] = (date_obj + pd.to_timedelta(df['Hour'], unit='h') + 
+                                            pd.to_timedelta(df['minute'], unit='m') + 
+                                            pd.to_timedelta(df['second'], unit='s'))
+            df.index = df['dateTime']
+            del(df['dateTime'])
+            return df
+        else:
+            return mydict
+
+if __name__ == "__main__":   
+    file_name = 'data_metop0220061203.csv'
+    df = load_meped_csv(file_name)
+    print(df)
