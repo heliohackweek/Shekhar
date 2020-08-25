3b60d615b53e03c347c54e8a7eadbb94da007b43
Mykhaylo Mike Shumko <msshumko@gmail.com>
2020-08-25 15:59:18 -0600

    Added a few more comments.

--- make_metop_scatter_matrix_plot.py
+++ make_metop_scatter_matrix_plot.py
@@ -10,12 +10,17 @@ import pandas as pd
 
 import read_csv_dict
 
+# Search for all the metop csv files.
 metop_dir = pathlib.Path('./csv_daily_data/')
 metop_paths = sorted(list(metop_dir.glob('data_metop*.csv')))
+
+# Make a list of the metop DataFrames and concatenate 
+# them into one. 
 df_list = [read_csv_dict.load_meped_csv(str(metop_path)) 
             for metop_path in metop_paths]
 df_concat = pd.concat(df_list)
 
+### Make plots! ###
 # df = df[df.mlt > 20 ] # Filter by MLT.
 # df.hist()
 pd.plotting.scatter_matrix(df_concat, alpha=0.2)
